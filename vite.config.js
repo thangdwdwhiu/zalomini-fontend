966c910ad1405dd398454a7035a96e5e3c2fece0
Xuan Thang <thangthitotnghiep@gmail.com>
2025-11-12 10:49:26 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true, // bind 0.0.0.0 để Render detect port
+    port: 4173, // Render sẽ override bằng $PORT
+    allowedHosts: ['zalomini-fontend.onrender.com'], // thêm host Render
+  },
 })
